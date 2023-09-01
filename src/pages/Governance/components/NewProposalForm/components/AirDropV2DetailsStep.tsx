0b2fc5a93abefbbd0192b7eea7a6c353ae428aa9
Pashmeena Noor <pashmeena.noor@metaschool.so>
2023-09-01 08:42:55 +0500

    centeralized

--- src/pages/Governance/components/NewProposalForm/components/AirDropV2DetailsStep.tsx
+++ src/pages/Governance/components/NewProposalForm/components/AirDropV2DetailsStep.tsx
@@ -3,9 +3,7 @@ import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { useForm } from '@q-dev/form-hooks';
-import { Calendar} from '@q-dev/q-ui-kit';
-import { keccak_256 as keccak256 } from 'js-sha3';
-import { MerkleTree } from 'merkletreejs';
+import { providers } from 'ethers';
 import { NewProposalForm } from 'typings/forms';
 
 import Button from 'components/Button';
@@ -13,20 +11,18 @@ import Input from 'components/Input';
 import { FormStep } from 'components/MultiStepForm';
 
 import { useNewProposalForm } from '../NewProposalForm';
-import { BigNumber } from "bignumber.js";
-import Web3 from 'web3';
-import { useProviderStore } from 'store/provider/hooks';
-import { providers, utils } from 'ethers';
 
+import { downloadJSON } from 'utils/download';
+import { buildTree, createLeafNodes } from 'utils/merkletree';
+import { toBN, wei } from 'utils/numbers';
+import { parameterType, required } from 'utils/validators';
 
 function AirDropV2DetailsStep () {
-  
   const [startTimestamp, setStartTimestamp] = useState<any>();
   const [endTimestamp, setEndTimestamp] = useState<any>();
 
   useEffect(() => {
     const fetchBlock = async () => {
-      // Set up the provider. Here, we're using a default provider which connects to multiple backends.
       const provider = new providers.JsonRpcProvider('https://rpc.qtestnet.org');
 
       // Fetch the latest block number.
@@ -47,82 +43,40 @@ function AirDropV2DetailsStep () {
   const [dynamicAddresses, setDynamicAddresses] = useState<string[]>([]);
   const [newAddress, setNewAddress] = useState('');
   const [addressError, setAddressError] = useState<string | null>(null);
-  
-  const addresses = [
-    "0xFDDD69Dbc041DeBE65A582BF6835A7D1172E94D2",
-    "0xABe215Fb79fB827978C82379d5974831E2FB5E0d",
-    "0xf503808EE7d381e14B5C39C4D28947D842fD7730",
-    "0x1A8a36bA1FF133bcFDD8C60bb01E511C363672B0"
-  ];
-  
-  function toBN(value: string | number | BigNumber | bigint) {
-    if (typeof value === "bigint") {
-      value = value.toString();
-    }
-  
-    return new BigNumber(value);
-  }
-
-  function wei(value: string | number | bigint, decimal: number = 18): bigint {
-    if (typeof value === 'number' || typeof value === 'bigint') {
-      value = value.toString();
-    }
-  
-    return utils.parseUnits(value as string, decimal).toBigInt();
-  }
-  
-  
-  function downloadJSON(data: any, filename: string): void {
-    const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = filename || 'download.json';
-    a.click();
-    URL.revokeObjectURL(url);
-}
 
   const getMerkleRoot = () => {
-    const leafNodes = ['0x1A8a36bA1FF133bcFDD8C60bb01E511C363672B0', '0x1A8a36bA1FF133bcFDD8C60bb01E511C363672B0'].map((address) =>
-      keccak256(
-        Buffer.from(address.replace('0x', ''), 'hex'),
-      ));
-    const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+    const leafNodes = createLeafNodes(dynamicAddresses);
+    const merkleTree = buildTree(leafNodes);
 
     // Exporting the tree as JSON
     const data = {
-      addresses: addresses.map((address) => address),
+      addresses: dynamicAddresses.map((address) => address),
       leafNodes: leafNodes,
       root: merkleTree.getHexRoot(),
     };
 
-    downloadJSON(data, "tree.json");
+    downloadJSON(data, 'tree.json');
     return merkleTree.getHexRoot();
   };
 
-  
   const form = useForm({
     initialValues: {
-      rewardToken: '0x536B3cEA28f86cBb90a9F9C3934f8220f230c5Bc',
+      rewardToken: '',
       amount: 1
     },
     validators: {
-      rewardToken: [],
-      amount: []
+      rewardToken: [required],
+      amount: [required]
     },
 
-
     onSubmit: async (form) => {
-      // if (dynamicAddresses.length < 2) {
-      //   setAddressError('Please provide at least 2 addresses.');
-      //   return; // Exit early so the form doesn't proceed
-      // }
-      console.log('form', form);
+      if (dynamicAddresses.length < 2) {
+        setAddressError('Please provide at least 2 addresses.');
+        return; // Exit early so the form doesn't proceed
+      }
       const merkleRoot = getMerkleRoot();
       const rewardAmount = wei(form.amount);
-      const updatedFormValues = { ...form, merkleRoot, startTimestamp, endTimestamp, rewardAmount};
-
-      console.log('updatedFormValues', updatedFormValues);
+      const updatedFormValues = { ...form, merkleRoot, startTimestamp, endTimestamp, rewardAmount };
       goNext(updatedFormValues as NewProposalForm);
     },
   });
@@ -138,8 +92,8 @@ function AirDropV2DetailsStep () {
         value={form.fields.rewardToken.value || ''}
         label={t('Reward Token Address')}
         placeholder={t('Enter reward token address')}
-        onChange={form.fields.rewardToken.onChange}
         error={form.fields.rewardToken.error}
+        onChange={form.fields.rewardToken.onChange}
       />
 
       <Input
@@ -147,8 +101,8 @@ function AirDropV2DetailsStep () {
         label={t('Reward Amount')}
         placeholder={t('Enter reward amount')}
         type="number"
-        onChange={form.fields.amount.onChange}
         error={form.fields.amount.error}
+        onChange={form.fields.amount.onChange}
       />
 
       {/* Dynamic addresses list */}
@@ -177,7 +131,7 @@ function AirDropV2DetailsStep () {
         value={newAddress}
         label={t('Enter New Address')}
         placeholder="Enter new address"
-        hint={"Enter atlest 2 addresses to create a Merkle Tree"}
+        hint={'Enter atlest 2 addresses to create a Merkle Tree'}
         onChange={value => setNewAddress(value)}
       />
 
